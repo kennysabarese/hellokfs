de2547aa934772151c8677129b24a17d2b930315
kennysabarese <sabark@ussasabarkm1.corp.emc.com>
2014-07-01 16:52:05 -0400

    Revert "first live change"
    This reverts commit b6a74865f23353ce08f98fc6b11057f707d67598.
    
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-echo "Hello world! now my own";
+<<<<<<< HEAD
+echo "Hello world! now my own1";
+=======
+echo "Hello world! now on github";
+>>>>>>> parent of b6a7486... first live change
 ?>
